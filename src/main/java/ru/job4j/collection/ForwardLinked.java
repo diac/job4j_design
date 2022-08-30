e6a02fe1b8b19fb774c68e747dc080b48990756a
diac <nikolai.gladkikh@gmail.com>
2022-08-30 12:25:55 +0300

    5. Очередь на двух стеках [#160]

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -36,6 +36,10 @@ public class ForwardLinked<T> implements Iterable<T> {
         return oldValue;
     }
 
+    public boolean isEmpty() {
+        return head == null;
+    }
+
     @Override
     public Iterator<T> iterator() {
         return new Iterator<>() {

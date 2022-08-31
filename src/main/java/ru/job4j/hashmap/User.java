fe146e006b38a1ffdae14daa0e2a2d404ace2d52
diac <nikolai.gladkikh@gmail.com>
2022-08-31 10:27:11 +0300

    1. Создать модель User [#999]

---
+++ src/main/java/ru/job4j/hashmap/User.java
@@ -0,0 +1,28 @@
+package ru.job4j.hashmap;
+
+import java.util.Calendar;
+
+public class User {
+
+    private final String name;
+    private final int children;
+    private final Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+}

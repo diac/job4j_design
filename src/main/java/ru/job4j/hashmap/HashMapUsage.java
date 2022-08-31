5e7d1f7fbfda33d14e1db86aa9c3b82fd1e051d9
diac <nikolai.gladkikh@gmail.com>
2022-08-31 11:48:26 +0300

    Инициализировать объекты одинаковыми данными при создании: 2. Без переопределения equals и hashCode [#1005 #338199]

--- src/main/java/ru/job4j/hashmap/HashMapUsage.java
+++ src/main/java/ru/job4j/hashmap/HashMapUsage.java
@@ -1,7 +1,6 @@
 package ru.job4j.hashmap;
 
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,20 +8,21 @@ public class HashMapUsage {
 
     public static void main(String[] args) {
         Map<User, Object> map = new HashMap<>();
-        User john = new User("John", 2, new GregorianCalendar(1980, Calendar.FEBRUARY, 20));
+        Calendar birthday = Calendar.getInstance();
+        User john = new User("John", 2, birthday);
         int johnHashCode = john.hashCode();
         int johnHash = johnHashCode ^ (johnHashCode >>> 16);
         int johnBucket = johnHash & 15;
-        User bob = new User("Bob", 3, new GregorianCalendar(1983, Calendar.APRIL, 2));
-        int bobHashCode = bob.hashCode();
-        int bobHash = bobHashCode ^ (bobHashCode >>> 16);
-        int bobBucket = bobHash & 15;
+        User john2 = new User("John", 2, birthday);
+        int john2HashCode = john2.hashCode();
+        int john2Hash = john2HashCode ^ (john2HashCode >>> 16);
+        int john2Bucket = john2Hash & 15;
         map.put(john, new Object());
-        map.put(bob, new Object());
+        map.put(john2, new Object());
         System.out.println(map);
         System.out.printf("John - хешкод: %s, хэш: %s, бакет: %s\n",
                 johnHashCode, johnHash, johnBucket);
-        System.out.printf("Bob - хешкод: %s, хэш: %s, бакет: %s",
-                bobHashCode, bobHash, bobBucket);
+        System.out.printf("John2 - хешкод: %s, хэш: %s, бакет: %s\n",
+                john2HashCode, john2Hash, john2Bucket);
     }
 }

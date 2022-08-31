328939519f21842fc376cc11887b2176ba3e2846
diac <nikolai.gladkikh@gmail.com>
2022-08-31 14:11:18 +0300

    5. Переопределить и equals и hashCode [#1002 #338234]

--- src/main/java/ru/job4j/hashmap/User.java
+++ src/main/java/ru/job4j/hashmap/User.java
@@ -1,6 +1,7 @@
 package ru.job4j.hashmap;
 
 import java.util.Calendar;
+import java.util.Objects;
 
 public class User {
 
@@ -36,5 +37,22 @@ public class User {
 
     public Calendar getBirthday() {
         return birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && name.equals(user.name) && birthday.equals(user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
     }
 }

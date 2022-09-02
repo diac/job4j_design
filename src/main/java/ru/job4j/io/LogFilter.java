12a18d5f61501bc38a3e2e40f9dace29ec6e210a
diac <nikolai.gladkikh@gmail.com>
2022-09-02 13:55:30 +0300

    0.3. BufferedReader. [#252489]

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,32 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogFilter {
+
+    public List<String> filter(String file) {
+        List<String> result = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            in.lines().forEach(line -> {
+                String[] chunks = line.split("\\s");
+                if ("404".equals(chunks[chunks.length - 2])) {
+                    result.add(line);
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        LogFilter logFilter = new LogFilter();
+        List<String> log = logFilter.filter("log.txt");
+        System.out.println(log);
+
+    }
+}

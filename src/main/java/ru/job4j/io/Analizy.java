ec642dc6dba3da8056f016cdfa0c6e70bbf48cb2
diac <nikolai.gladkikh@gmail.com>
2022-09-03 10:13:24 +0300

    Упростить код: 2. Анализ доступности сервера. [#859 #338995]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -9,18 +9,14 @@ public class Analizy {
                 BufferedReader in = new BufferedReader(new FileReader(source));
                 BufferedWriter out = new BufferedWriter(new FileWriter(target))
         ) {
-            boolean errorSection = false;
+            boolean errorSection = true;
             while (in.ready()) {
                 String[] line = in.readLine().split(" ", 2);
                 var hasErrorCode = "400".equals(line[0]) || "500".equals(line[0]);
-                if (hasErrorCode && !errorSection) {
-                    out.append(line[1]);
-                    errorSection = true;
-                } else if (!hasErrorCode && errorSection) {
-                    out.append(";")
-                            .append(line[1])
-                            .append("\n");
-                    errorSection = false;
+                if (hasErrorCode == errorSection) {
+                    out.append(line[1])
+                            .append(errorSection ? ";" : System.lineSeparator());
+                    errorSection = !errorSection;
                 }
             }
         } catch (IOException e) {

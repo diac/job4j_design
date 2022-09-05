5627bef82f41ac236ac702b41f47143e1f27baa4
diac <nikolai.gladkikh@gmail.com>
2022-09-05 13:26:30 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -1,20 +1,36 @@
 package ru.job4j.io;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Dir {
 
     public static void main(String[] args) {
-        File file = new File("c:\\projects");
+        LaunchParams launchParams = validateArguments(args);
+        System.out.printf("size : %s%n", launchParams.file.getTotalSpace());
+        Arrays.stream(Objects.requireNonNull(launchParams.file.listFiles()))
+                .filter(file -> file.getName().endsWith("." + launchParams.extension))
+                .forEach(System.out::println);
+    }
+
+    private static LaunchParams validateArguments(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        if (args.length < 2) {
+            throw new IllegalArgumentException("File extension is required");
+        }
+        File file = new File(args[0]);
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        System.out.printf("size : %s%n", file.getTotalSpace());
-        for (File subfile : file.listFiles()) {
-            System.out.printf("%s: %s\n", subfile.getName(), !subfile.isDirectory() ? subfile.length() : "(directory)");
-        }
+        return new LaunchParams(file, args[1]);
+    }
+
+    private record LaunchParams(File file, String extension) {
     }
 }
